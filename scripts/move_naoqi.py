24bcd041ecdf5e760e6c40c0b454e6c6dd4d9889
OnisBrown <onis.Brown@hotmail.co.uk>
2018-03-09 22:32:06 +0000

    fixed nodding added comments

--- scripts/move_naoqi.py
+++ scripts/move_naoqi.py
@@ -13,21 +13,30 @@ from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 class Mover:
 	def __init__(self):
 		rospy.init_node('mover', anonymous=True)
+		
+		# publishers for each robot body part used
 		self.ph = rospy.Publisher('/nao_dcm/Head_controller/command', JointTrajectory, queue_size=100)
 		self.pal = rospy.Publisher('/nao_dcm/LeftArm_controller/command', JointTrajectory, queue_size=100)
 		self.par = rospy.Publisher('/nao_dcm/RightArm_controller/command', JointTrajectory, queue_size=100)
+		
 		self.r = rospy.Rate(100)
+		
+		# message objects and default message intervals
 		self.jt = JointTrajectory()
 		self.jtp = JointTrajectoryPoint()
 		self.interval = 0.5
+		
+		# joint names for each body part used by related message
 		self.headJ = ['HeadPitch', 'HeadYaw']
 		self.LArmJ = ['LElbowRoll', 'LElbowYaw', 'LShoulderPitch', 'LShoulderRoll', 'LWristYaw']
 		self.RArmJ = ['RElbowRoll', 'RElbowYaw', 'RShoulderPitch', 'RShoulderRoll', 'RWristYaw']
 		print "Nao mover node ready"
 
+	# publisher method that accepts a publisher object and uses it
 	def pub(self, p):
 		p.publish(self.jt)
 
+	# movement method for creating movement messages
 	def move(self, goal, p):
 		try:
 			self.move_setup()
@@ -38,7 +47,7 @@ class Mover:
 
 		except KeyboardInterrupt:
 			sys.exit()
-
+	# method for setting up move messages by setting all movement parameters to null
 	def move_setup(self):
 		try:
 			self.jt.points = []
@@ -50,7 +59,7 @@ class Mover:
 
 		except KeyboardInterrupt:
 			sys.exit()
-
+	# method for returning the robot to a neutral position
 	def body_reset(self):
 		try:
 			self.move_setup()
@@ -68,8 +77,8 @@ class Mover:
 		except KeyboardInterrupt:
 			sys.exit()
 
-
-	def head_nod(self, sad):
+	# method for making the robot nodding it's head
+	def head_nod(self, mood):
 		try:
 			self.jt.joint_names = ['HeadPitch', 'HeadYaw']
 			p = self.ph
@@ -77,10 +86,13 @@ class Mover:
 				self.interval = 0.4
 				goal = [0.0, 0.0]
 				self.move(goal, p)
+				rospy.sleep(i)
 				goal[0] += 0.6
 				self.move(goal, p)
+				rospy.sleep(i)
 				goal[1] -= 0.6
 				self.move(goal, p)
+				rospy.sleep(i)
 
 			else:
 				self.interval = 0.5
@@ -93,46 +105,37 @@ class Mover:
 
 		except KeyboardInterrupt:
 			sys.exit()
-
-	def head_shake(self, sad):
+	
+	# method for making the robot shake it's head
+	def head_shake(self, mood):
 		try:
 			self.jt.joint_names = ['HeadPitch', 'HeadYaw']
 			p = self.ph
 			i = self.interval
-			if sad:
+			if mood >= 0.5:
 				i = 0.5
 				goal = [0.4, 1.0]
-				self.move(goal, p)
-				rospy.sleep(i)
-				goal[1] -= 1
-				self.move(goal, p)
-				rospy.sleep(i)
-				goal[1] -= 1
-				self.move(goal, p)
-				rospy.sleep(i)
-				goal[1] += 1
-				self.move(goal, p)
-				rospy.sleep(i)
 
 			else:
 				i = 0.2
 				goal = [0.0, 1.0]
-				self.move(goal, p)
-				rospy.sleep(i)
-				goal[1] -= 1
-				self.move(goal, p)
-				rospy.sleep(i)
-				goal[1] -= 1
-				self.move(goal, p)
-				rospy.sleep(i)
-				goal[1] += 1
-				self.move(goal, p)
-				rospy.sleep(i)
+			
+			self.move(goal, p)
+			rospy.sleep(i)
+			goal[1] -= 1
+			self.move(goal, p)
+			rospy.sleep(i)
+			goal[1] -= 1
+			self.move(goal, p)
+			rospy.sleep(i)
+			goal[1] += 1
+			self.move(goal, p)
+			rospy.sleep(i)
 
 		except KeyboardInterrupt:
 			sys.exit()
 
-
+# message on program exit
 def my_hook():
 	print "shutting down"
 
@@ -140,6 +143,7 @@ def my_hook():
 rospy.on_shutdown(my_hook)
 
 test = Mover()
+mood = 0.5
 
 while not rospy.is_shutdown():
 	try:
